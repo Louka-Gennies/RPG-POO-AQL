96d145269e49c3a44075e4ebe4088750dd534a3c
Louka-Gennies <146700433+Louka-Gennies@users.noreply.github.com>
2024-03-25 10:08:21 +0100

    class.ts

---
+++ Class.ts
@@ -0,0 +1,25 @@
+import Character from "./Character.ts";
+
+class Warrior extends Character {
+    
+}
+
+class Mage extends Character {
+
+}
+
+class Paladin extends Character {
+
+}
+
+class Barbarian extends Character {
+
+}
+
+class Priest extends Character {
+
+}
+
+class thief extends Character {
+
+}
