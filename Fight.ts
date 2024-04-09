cf7985a2faa8090343055a11f7de2d5a8548252b
Louka-Gennies <146700433+Louka-Gennies@users.noreply.github.com>
2024-04-09 10:53:11 +0200

    manage the error on the menu

--- Fight.ts
+++ Fight.ts
@@ -42,6 +42,111 @@ export default class Fight {
     team.splice(index, 1);
   }
 
+  async ItemManager(character: Character, invent: Inventory) {
+    console.clear();
+    const item = character.ItemMenu(invent);
+    switch (item) {
+      case 0:
+        invent.items[item].quantity -= 1;
+        console.clear();
+        const healed = character.heal(50);
+        console.log(`You healed yourself of ${healed} HP\n`);
+        await new Promise((r) => setTimeout(r, 1000));
+        break;
+      case 1:
+        console.clear();
+        const choicesFragment: string[] = [];
+        for (let i = 0; i < this.team1.length; i++) {
+          choicesFragment.push(this.team1[i].name);
+        }
+        const menuFragment = new Menu("Choose a character to revive: ", choicesFragment);
+        const targetFragment = menuFragment.askQuestion();
+        if (this.team1[targetFragment].currentHP > 0) {
+          invent.items[item].quantity -= 1;
+          console.clear();
+          const healed = this.team1[targetFragment].heal(50);
+          console.log(`${this.team1[targetFragment].name} has been healed of ${healed} HP\n`);
+          await new Promise((r) => setTimeout(r, 1000));
+          break;
+        } else {
+          invent.items[item].quantity -= 1;
+          console.clear();
+          const healed = this.team1[targetFragment].res(20);
+          console.log(`${this.team1[targetFragment].name} has been revived with ${healed} HP\n`);
+          await new Promise((r) => setTimeout(r, 1000));
+          break;
+        }
+      case 2:
+        console.clear();
+        const choicesHalf: string[] = [];
+        for (let i = 0; i < this.team1.length; i++) {
+          choicesHalf.push(this.team1[i].name);
+        }
+        const menuHalf = new Menu("Choose a character to revive: ", choicesHalf);
+        const targetHalf = menuHalf.askQuestion();
+        if (this.team1[targetHalf].currentHP > 0) {
+          invent.items[item].quantity -= 1;
+          console.clear();
+          const healed = this.team1[targetHalf].heal(100);
+          console.log(`${this.team1[targetHalf].name} has been healed of ${healed} HP\n`);
+          break;
+        } else {
+          invent.items[item].quantity -= 1;
+          console.clear();
+          const healed = this.team1[targetHalf].res(100);
+          console.log(`${this.team1[targetHalf].name} has been revived with ${healed} HP\n`);
+          break;
+        }
+        case 3:
+          if (character instanceof Mage) {
+            console.clear();
+            character.rechargeMana(30);
+            invent.items[item].quantity -= 1;
+            break;
+          } else {
+            console.clear();
+            console.log("You can't use this item on this character");
+            console.clear();
+            this.ItemManager(character, invent);
+            break;
+          }
+        default:
+          console.clear();
+          console.log("You need to choose a Number between 1 and 4");
+          console.clear();
+          this.ItemManager(character, invent);
+          break;
+    }
+  }
+
+  async ActionManager(character: Character, team: Character[], invent: Inventory) {
+    const action = character.actionMenu();
+            switch (action) {
+              case 0:
+                character.attack(this.team2);
+                await new Promise((r) => setTimeout(r, 1000));
+                break;
+              case 1:
+                if (character.name === "Priest") {
+                  console.clear();
+                  character.specialAttack(this.team1, invent);
+                  break;
+                } else {
+                  console.clear();
+                  character.specialAttack(this.team2, invent);
+                  break;
+                }
+              case 2:
+                this.ItemManager(character, invent);
+                break;
+              default:
+                console.clear();
+                console.log("You need to choose a Number between 1 and 3\n");
+                this.ActionManager(character, team, invent);
+                break;
+            }
+  }
+
   async fight(invent : Inventory) : Promise<boolean> {
     const team1Print = this.team1
     const team2Print = this.team2
@@ -65,101 +170,7 @@ export default class Fight {
           character.active = true;
           character.stats(team1Print, team2Print);
           if (this.team1.includes(character)) {
-            const action = character.actionMenu();
-            switch (action) {
-              case 0:
-                character.attack(this.team2);
-                await new Promise((r) => setTimeout(r, 1000));
-                break;
-              case 1:
-                if (character.name === "Priest") {
-                  console.clear();
-                  character.specialAttack(this.team1, invent);
-                  await new Promise((r) => setTimeout(r, 1000));
-                  break;
-                } else {
-                  console.clear();
-                  character.specialAttack(this.team2, invent);
-                  await new Promise((r) => setTimeout(r, 1000));
-                  break;
-                }
-              case 2:
-                console.clear();
-                const item = character.ItemMenu(invent);
-                switch (item) {
-                  case 0:
-                    invent.items[item].quantity -= 1;
-                    console.clear();
-                    const healed = character.heal(50);
-                    console.log(`You healed yourself of ${healed} HP\n`);
-                    await new Promise((r) => setTimeout(r, 1000));
-                    break;
-                  case 1:
-                    console.clear();
-                    const choicesFragment: string[] = [];
-                    for (let i = 0; i < this.team1.length; i++) {
-                      choicesFragment.push(this.team1[i].name);
-                    }
-                    const menuFragment = new Menu("Choose a character to revive: ", choicesFragment);
-                    const targetFragment = menuFragment.askQuestion();
-                    if (this.team1[targetFragment].currentHP > 0) {
-                      invent.items[item].quantity -= 1;
-                      console.clear();
-                      const healed = this.team1[targetFragment].heal(50);
-                      console.log(`${this.team1[targetFragment].name} has been healed of ${healed} HP\n`);
-                      await new Promise((r) => setTimeout(r, 1000));
-                      break;
-                    } else {
-                      invent.items[item].quantity -= 1;
-                      console.clear();
-                      const healed = this.team1[targetFragment].res(20);
-                      console.log(`${this.team1[targetFragment].name} has been revived with ${healed} HP\n`);
-                      await new Promise((r) => setTimeout(r, 1000));
-                      break;
-                    }
-                  case 2:
-                    console.clear();
-                    const choicesHalf: string[] = [];
-                    for (let i = 0; i < this.team1.length; i++) {
-                      choicesHalf.push(this.team1[i].name);
-                    }
-                    const menuHalf = new Menu("Choose a character to revive: ", choicesHalf);
-                    const targetHalf = menuHalf.askQuestion();
-                    if (this.team1[targetHalf].currentHP > 0) {
-                      invent.items[item].quantity -= 1;
-                      console.clear();
-                      const healed = this.team1[targetHalf].heal(100);
-                      console.log(`${this.team1[targetHalf].name} has been healed of ${healed} HP\n`);
-                      await new Promise((r) => setTimeout(r, 1000));
-                      break;
-                    } else {
-                      invent.items[item].quantity -= 1;
-                      console.clear();
-                      const healed = this.team1[targetHalf].res(100);
-                      console.log(`${this.team1[targetHalf].name} has been revived with ${healed} HP\n`);
-                      await new Promise((r) => setTimeout(r, 1000));
-                      break;
-                    }
-                    case 3:
-                      if (character instanceof Mage) {
-                        console.clear();
-                        character.rechargeMana(30);
-                        invent.items[item].quantity -= 1;
-                        await new Promise((r) => setTimeout(r, 1000));
-                        break;
-                      } else {
-                        console.clear();
-                        invent.items[item].quantity -= 1;
-                        console.log("You can't use this item on this character");
-                        await new Promise((r) => setTimeout(r, 1000));
-                        console.log("You stare at the item for a moment, then it diseappears..")
-                        await new Promise((r) => setTimeout(r, 1000));12
-                        break;
-                      }
-                }
-                await new Promise((r) => setTimeout(r, 1000));
-                break;
-            }
+            this.ActionManager(character, this.team1, invent);
           } else {
             console.clear();
             character.attack(this.team1);
